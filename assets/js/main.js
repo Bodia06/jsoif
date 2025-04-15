7500981226b4c2a29b9ece6322d8ac5b64ded9a1
Bohdan <bohdan.verbetskyi@gmail.com>
2025-04-15 15:10:01 +0200

    Fix file main.js & header.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 //Task_1
-const isDay = Boolean(prompt('Is it day? (true/false)'))
+const isDay = prompt('Is it day? (true/false)')
 if (isDay === 'true') {
 	console.log('Світла тема')
-	document.body.style.backgroundColor = '#000'
-	document.body.style.color = '#fff'
+	document.body.style.backgroundColor = '#fff'
+	document.body.style.color = '#000'
 } else {
 	console.log('Темна тема')
-	document.body.style.backgroundColor = '#fff'
-	document.body.style.scolor = '#000'
+	document.body.style.backgroundColor = '#000'
+	document.body.style.color = '#fff'
 }
 
 //Task_2
@@ -21,77 +21,55 @@ if (!isOnline) {
 const buyMoney = Number(
 	prompt('Введіть суму грошей, яку ви готові витратити на покупки')
 )
-if (buyMoney > 500) {
-	console.log('You buy all items: ', buyMoney - buyMoney * 0.3)
+if (buyMoney > 500 && buyMoney <= 800) {
+	console.log('You buy all items: ', buyMoney - buyMoney * 0.03)
 } else if (buyMoney > 800) {
-	console.log('You buy all items: ', buyMoney - buyMoney * 0.5)
+	console.log('You buy all items: ', buyMoney - buyMoney * 0.05)
 } else {
 	console.log('You buy all items: ', buyMoney)
 }
 //Task_4
-document.addEventListener('DOMContentLoaded', () => {
-	const userMaleImg =
-		'https://icons.veryicon.com/png/o/internet--web/web-interface-flat/6606-male-user.png'
-	const userFemaleImg =
-		'https://brandeps.com/icon-download/U/User-female-icon-02.png'
-	const userSection = document.querySelector('.header-content__registration')
-	const linkRegistration = document.querySelector(
-		'.header-content__registration-list'
-	)
-	const linkLogin = document.querySelector(
-		'.header-content__registration-list .login a'
-	)
+const userMaleImg =
+	'https://icons.veryicon.com/png/o/internet--web/web-interface-flat/6606-male-user.png'
+const userFemaleImg =
+	'https://brandeps.com/icon-download/U/User-female-icon-02.png'
+const userSection = document.querySelector('.header-content__registration')
+const linkRegistration = document.querySelector(
+	'.header-content__registration-list'
+)
+const linkLogin = document.querySelector(
+	'.header-content__registration-list .login a'
+)
 
-	linkLogin.addEventListener('click', (event) => {
-		event.preventDefault()
+linkLogin.addEventListener('click', (event) => {
+	event.preventDefault()
 
-		const userName = prompt('Enter your name')
-		const isMale = prompt('Are you male? (true/false)').toLowerCase()
+	const userName = prompt('Enter your name')
+	const isMale = prompt('Are you male? (true/false)').toLowerCase() === 'true'
 
-		if (isMale === 'true') {
-			linkRegistration.classList.add('unactive')
-			const userImg = document.createElement('img')
-			userImg.src = userMaleImg
-			userImg.style.border = '3px solid blue'
-			userImg.style.borderRadius = '50%'
-			userImg.style.width = '50px'
-			userImg.style.height = '50px'
+	const borderColor = isMale ? 'blue' : 'red'
+	const userImgSrc = isMale ? userMaleImg : userFemaleImg
 
-			const userNameElement = document.createElement('span')
-			userNameElement.textContent = userName
-			userNameElement.style.marginLeft = '10px'
+	linkRegistration.classList.add('unactive')
+	const userImg = document.createElement('img')
+	userImg.src = userImgSrc
+	userImg.style.border = '2px solid ' + borderColor
+	userImg.style.borderRadius = '50%'
+	userImg.style.width = '50px'
+	userImg.style.height = '50px'
 
-			const userContainer = document.createElement('div')
-			userContainer.style.display = 'flex'
-			userContainer.style.alignItems = 'center'
-			userContainer.appendChild(userImg)
-			userContainer.appendChild(userNameElement)
+	const userNameElement = document.createElement('span')
+	userNameElement.textContent = userName
+	userNameElement.style.marginLeft = '10px'
 
-			userSection.innerHTML = ''
-			userSection.appendChild(userContainer)
-		} else if (isMale === 'false') {
-			linkRegistration.classList.add('unactive')
-			const userImg = document.createElement('img')
-			userImg.src = userFemaleImg
-			userImg.style.border = '3px solid red'
-			userImg.style.borderRadius = '50%'
-			userImg.style.width = '50px'
-			userImg.style.height = '50px'
+	const userContainer = document.createElement('div')
+	userContainer.style.display = 'flex'
+	userContainer.style.alignItems = 'center'
+	userContainer.appendChild(userImg)
+	userContainer.appendChild(userNameElement)
 
-			const userNameElement = document.createElement('span')
-			userNameElement.textContent = userName
-			userNameElement.style.marginLeft = '10px'
-
-			const userContainer = document.createElement('div')
-			userContainer.style.display = 'flex'
-			userContainer.style.alignItems = 'center'
-			userContainer.appendChild(userImg)
-			userContainer.appendChild(userNameElement)
-
-			userSection.innerHTML = ''
-			userSection.appendChild(userContainer)
-		}
-	})
+	userSection.innerHTML = ''
+	userSection.appendChild(userContainer)
 })
 
 //Task_5
